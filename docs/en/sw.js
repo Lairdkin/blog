f8fcfb22d03dd27b0e05df882181f59e8c43c390
mx <1979646055@qq.com>
2021-06-15 17:55:45 +0800

    文章发布：06/15/2021 17:55:45

--- docs/en/sw.js
+++ docs/en/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "1623750603";
+const CACHE_VERSION = "1623750944";
 importScripts('./workbox-sw.js')
 
 //2
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "1623750604";
+const CACHE_VERSION = "1623750945";
 importScripts('./workbox-sw.js')
 
 //2
