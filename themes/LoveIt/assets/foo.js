26ae407091734978537810d58992002da54fa02b
mx <1979646055@qq.com>
2021-06-16 03:05:29 +0800

    test

---
+++ themes/LoveIt/assets/foo.js
@@ -0,0 +1,73 @@
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+  console.log('before install prompt')
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showAddToHomeScreen();
+});
+
+function showAddToHomeScreen() {
+  tata.info("<span>使<i>萝卜炖鱼丸的博客</i>可以离线访问？</span>",'<button class="btn-flat toast-action" onclick="addToHomeScreen()">Yes</button>')
+}
+
+
+
+function addToHomeScreen() {
+  deferredPrompt.prompt();  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then(function (choiceResult) {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      // 释放不再有用的deferredPrompt对象
+      deferredPrompt = null;
+    });
+}
+
+
+fetch("https://v1.hitokoto.cn/?c=a").then(res => {
+  return res.json()
+}).then(res => {
+  try {
+    if (res.from_who === null) {
+
+      document.getElementById("oneword").innerText = res.hitokoto + "     ————《" + res.from + "》"
+    } else {
+      document.getElementById("oneword").innerText = res.hitokoto + "     ————" + res.from_who + "《" + res.from + "》"
+    }
+    console.log(res.hitokoto)
+  } catch (err) { }
+  try {
+    document.getElementById("busuanzi_container_site_uv").innerText = res.uv
+  } catch (err) { }
+  try {
+    document.getElementById("busuanzi_container_site_pv").innerText = res.total
+  } catch (err) { }
+})
+
+
+let parm1 = ''
+if (document.cookie.indexOf("uv=false") == -1) {
+  parm1 = "&uv=true"
+  var t = new Date(new Date().getTime() + 1000 * 60 * 60 * 24);
+  document.cookie = "uv=false; expires=" + t.toGMTString() + ";path=/";
+}
+fetch("https://analytics.radishkin.workers.dev/api?url=" + window.btoa(location.href) + parm1).then(res => {
+  return res.json()
+}).then(res => {
+  try {
+    document.getElementById("busuanzi_value_page_pv").innerText = res.pv
+  } catch (err) { }
+  try {
+    document.getElementById("busuanzi_container_site_uv").innerText = res.uv
+  } catch (err) { }
+  try {
+    document.getElementById("busuanzi_container_site_pv").innerText = res.total
+  } catch (err) { }
+})
---
+++ themes/LoveIt/layouts/index.sw.js
@@ -0,0 +1,104 @@
+const CACHE_VERSION = "{{ now.Unix }}";
+importScripts('./workbox-sw.js')
+
+//2
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`)
+    workbox.setConfig({ debug: false })
+
+    //self.skipWaiting()
+    workbox.core.clientsClaim();
+
+
+    self.addEventListener("message",(event)=>{
+        if(event.data && event.data.type === "SKIP_WAITING"){
+            self.skipWaiting()
+        }
+    })
+
+
+    workbox.routing.registerRoute(
+        /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "images",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 1000,
+                    maxAgeSeconds: 60 * 60 * 24 * 30
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200]
+                })
+            ],
+        })
+    );
+
+
+
+    workbox.routing.registerRoute(
+        /^https:\/\/utterances\.radish\.cloud/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "comment",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 1000,
+                    maxAgeSeconds: 60 * 60 * 24 * 30
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200]
+                })
+            ]
+        })
+    );
+
+
+    workbox.routing.registerRoute(
+        /^https:\/\/cdn\.jsdelivr\.net/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "static-libs",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 1000,
+                    maxAgeSeconds: 60 * 60 * 24 * 30
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200]
+                })
+            ]
+        })
+    );
+
+    
+    workbox.precaching.precacheAndRoute([
+        { url: '/index.html', revision: CACHE_VERSION },
+        { url: '/posts/index.html', revision: CACHE_VERSION },
+        { url: '/categories/index.html', revision: CACHE_VERSION },
+        { url: '/about/index.html', revision: CACHE_VERSION },
+        { url: '/index.json', revision: CACHE_VERSION },
+        { url: '/links/index.html',revision: CACHE_VERSION}
+        // ... other entries ...
+    ]);
+
+
+    const matchFunction = ({ url }) => {
+        return /\.(?:js|css|json||woff2)$/.test(url['href']) && !/variable.js/.test(url['href']) && !/sw.js/.test(url['href']);
+    };
+
+    workbox.routing.registerRoute(
+        matchFunction,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'static-resources'
+        }),
+        
+    )
+
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets'
+        })
+    )
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`)
+}
